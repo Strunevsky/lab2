04823dcb9b1a5e621276c13e03b564e8e389abb6
Strunevsky <98808799+Strunevsky@users.noreply.github.com>
2022-02-21 08:58:39 +0500

    Add files via upload

---
+++ helloworld/ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             double a=2, b=3,c ;
+            Console.WriteLine("Ввести А");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввести Б");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выполнить операцию +");
+            Console.WriteLine("Выполнить операцию -");
+            Console.WriteLine("Выполнить операцию *");
+            Console.WriteLine("Выполнить операцию /");
+            char rez = char.Parse(Console.ReadLine());
+            if (rez == '+')
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+            }
+           else if (rez == '-')
+            {
+                c = a - b;
+                Console.WriteLine(c);
+            }
+            else if (rez == '*')
+            {
+                c = a * b;
+                Console.WriteLine(c);
+            }
+            else if (rez == '/')
+            {
+                c = a / b;
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
